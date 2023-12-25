dda94c15371f3a4cfa17fb2efd064968f9444d85
monica255 <monicasucianto123@gmail.com>
2023-12-25 20:47:20 +0800

    Initial Commit

---
+++ Games/GamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GamesApp.swift
+//  Games
+//
+//  Created by Monica Sucianto on 25/12/23.
+//
+
+import SwiftUI
+
+@main
+struct GamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
